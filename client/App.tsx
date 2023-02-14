39a1c2db12b5c83d38e17b0cb6aec7e9e4c36476
fradotech <fradotech.id@gmail.com>
2023-02-14 22:14:46 +0700

    Init loading provider

--- client/App.tsx
+++ client/App.tsx
@@ -2,14 +2,17 @@ import { ConfigProvider } from 'antd'
 import locale from 'antd/locale/id_ID'
 import 'dayjs/locale/id'
 import React from 'react'
+import { LoadingProvider } from './Contexts/Loading'
 import Routers from './Routers'
 import { globalThemeConfig } from './utils/theme'
 
 const App: React.FC = () => {
   return (
-    <ConfigProvider theme={globalThemeConfig} locale={locale}>
-      <Routers />
-    </ConfigProvider>
+    <LoadingProvider>
+      <ConfigProvider theme={globalThemeConfig} locale={locale}>
+        <Routers />
+      </ConfigProvider>
+    </LoadingProvider>
   )
 }
 
