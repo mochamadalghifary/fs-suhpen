c7aead63f09fb4b348c9dea0846bb13664915c46
fradotech <fradotech.id@gmail.com>
2023-02-15 11:50:38 +0700

    Remove loading provider

--- client/App.tsx
+++ client/App.tsx
@@ -2,17 +2,14 @@ import { ConfigProvider } from 'antd'
 import locale from 'antd/locale/id_ID'
 import 'dayjs/locale/id'
 import React from 'react'
-import { LoadingProvider } from './Contexts/Loading'
 import Routers from './Routers'
 import { globalThemeConfig } from './utils/theme'
 
 const App: React.FC = () => {
   return (
-    <LoadingProvider>
-      <ConfigProvider theme={globalThemeConfig} locale={locale}>
-        <Routers />
-      </ConfigProvider>
-    </LoadingProvider>
+    <ConfigProvider theme={globalThemeConfig} locale={locale}>
+      <Routers />
+    </ConfigProvider>
   )
 }
 
