818fa67027df7d4a6a0c4102b51577b91132b363
fradotech <fradotech.id@gmail.com>
2023-02-18 18:13:02 +0700

    Add logo at layout

--- client/Layouts/MainLayout/ProfileBar.tsx
+++ client/Layouts/MainLayout/ProfileBar.tsx
@@ -1,6 +1,6 @@
 import { UserOutlined } from '@ant-design/icons'
 import { IUser } from '@server/modules/iam/user/infrastructure/user.interface'
-import { Avatar, Space, Typography } from 'antd'
+import { Avatar, Image, Space, Typography } from 'antd'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Route } from '../../Enums/Route'
@@ -15,19 +15,27 @@ const { Text } = Typography
 
 const ProfileBar: React.FC<IProps> = (props: IProps) => {
   return (
-    <div
-      style={{
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        height: '1000x',
-        padding: '8px 16px',
-        marginTop: '30px',
-        marginBottom: '20px',
-      }}
-    >
-      <Link to={Route.Profile}>
+    <>
+      <Image
+        src="https://avatars.githubusercontent.com/u/55073493?v=4"
+        preview={false}
+        style={{
+          width: '75%',
+          paddingLeft: '40px',
+        }}
+      />
+      <div style={{ height: '10px' }}></div>
+      <Link
+        to={Route.Profile}
+        style={{
+          width: '100%',
+          backgroundColor: '#eeeeee',
+          borderRadius: '20px 0px 0px 20px',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '10px 10px',
+        }}
+      >
         <Space size="small">
           <Avatar icon={<UserOutlined />} src={props?.user.avatar} />
 
@@ -36,7 +44,7 @@ const ProfileBar: React.FC<IProps> = (props: IProps) => {
           </Space.Compact>
         </Space>
       </Link>
-    </div>
+    </>
   )
 }
 
