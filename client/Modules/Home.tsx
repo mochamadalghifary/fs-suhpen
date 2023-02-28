90e15fca89b3e302b6cc30eefa2c67ab034950de
fradotech <fradotech.id@gmail.com>
2023-02-28 19:12:04 +0700

    Add homepage image

--- client/Modules/Home.tsx
+++ client/Modules/Home.tsx
@@ -1,22 +1,29 @@
-import { Button, Result } from 'antd'
+import { Image, Layout } from 'antd'
+import { Content, Header } from 'antd/es/layout/layout'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { Route } from '../Enums/Route'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => {
-  const navigate = useNavigate()
-
   return (
-    <Result
-      status="500"
-      title="500"
-      subTitle="Slicing Home"
-      extra={
-        <Button onClick={() => navigate(Route.Login)} type="primary">
-          Login
-        </Button>
-      }
-    />
+    <Layout>
+      <Header
+        style={{
+          background: '#6FAFAB',
+          justifyContent: 'space-around',
+        }}
+      >
+        <Link to="#">Home</Link>
+        <Link to="#">Profile</Link>
+      </Header>
+      <Content
+        style={{
+          padding: '20px 20px',
+          overflow: 'auto',
+        }}
+      >
+        <Image src="https://suhpen.up.railway.app/images/home.jpg" />
+      </Content>
+    </Layout>
   )
 }
 export default Home
--- client/Routers.tsx
+++ client/Routers.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Route as ERoute } from './Enums/Route'
 import LayoutMain from './Layouts/MainLayout/LayoutMain'
 import Dashboard from './Modules/Dashboard/Dashboard'
 import DashboardRoute from './Modules/Dashboard/Dashboard.route'
+import Home from './Modules/Home'
 import { authAction } from './Modules/Iam/Auth/auth.action'
 import AuthRoute from './Modules/Iam/Auth/Auth.route'
 import ProfileRoute from './Modules/Iam/Profile/Profile.route'
@@ -14,12 +16,12 @@ const Routers: React.FC = () => (
   <>
     <BrowserRouter>
       <Routes>
-        {/* <Route path={HttpRoute.Home} element={<Home />} /> */}
+        <Route path={ERoute.Home} element={<Home />} />
         {AuthRoute}
         {!user && <Route path="*" element={<Unauthorized />} />}
       </Routes>
 
-      {user && (
+      {user && location.pathname != ERoute.Home && (
         <LayoutMain>
           <Routes>
             {DashboardRoute}
